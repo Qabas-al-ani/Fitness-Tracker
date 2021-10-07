c33e6ba8bdbddf1c715d7970706b3bbfef6caa18
Qabas al ani <doctorqa@yahoo.com>
2021-10-07 13:13:55 -0400

    added the required the libraries and the port for the locahost and the heroku

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 // declaring the application
  const app = express();
+
+ app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
